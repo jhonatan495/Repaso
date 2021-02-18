b013d68d20b09f851ae9238371c9ceea14f6677d
jhonatan495 <78876401+jhonatan495@users.noreply.github.com>
2021-02-17 20:28:28 -0500

    Update punto4.java

--- punto4.java
+++ punto4.java
@@ -4,23 +4,32 @@ package com.mycompany.actividadderepaso;
 import java.util.Scanner;
 
 
-public class punto4 {
+public class punto_Potencia {
 
-    
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int base=0,exponente=0,resultado=0;
+        System.out.println("Ingrese la base");
+        base=leer.nextInt();
+        System.out.println("Ingrese la exponente");
+        exponente=leer.nextInt();
         
-           Scanner leer = new Scanner(System.in);
-        int total=1;
-        System.out.println("Ingrese dos numeros para la potencia ");
-        System.out.println("Ingrese el numero n:");
-        int num1 = leer.nextInt();
-        System.out.println("Ingrese el numero m:");
-        int num2=leer.nextInt();
+        resultado = potencia(base, exponente);
+        System.out.println("la potencia es " + resultado);
         
-        for (int i = 0; i < num2; i++) {
-             total = num1*total;
-        }
-        System.out.println("El resultado es: "+total);
     }
-    
+
+    public static int Potencia(int base, int exponente) {
+        int resultado = base;
+        int count=0;
+        for (int i = 0; i < base; i++) {
+            if(count>0){
+                for (int j = 0; j < count; j++) {
+                    resultado+=resultado+base;
+                }
+            }
+        }
+        return resultado;
+    }
+
 }
